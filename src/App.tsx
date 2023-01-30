2ca1ab4bb7fa9096ac6bbeb6114396c075e05022
Finalcreature <finalnadav@gmail.com>
2023-01-30 15:38:55 +0200

    Manage to connect to user to back

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:8080");
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Cluedo!</h1>
     </div>
   );

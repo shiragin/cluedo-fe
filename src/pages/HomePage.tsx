0c5e30a344065dd5d3dd164281c97f7bc0e35cb5
Shira Ginat <shiragin@gmail.com>
2023-01-31 13:57:46 +0200

    Add card styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import SuspectCard from '../components/Cards/ClueCard';
 // import { RxMagnifyingGlass } from 'react-icons/rx';
-import { useGameContext } from "../Context/Context";
-import "../Styling/Homepage.scss";
+import { useGameContext } from '../Context/Context';
+import '../Styling/Homepage.scss';
 
 function HomePage() {
-  const [nickName, setNickname] = useState("");
+  const [nickName, setNickname] = useState('');
 
   const { onAddUser } = useGameContext();
 
@@ -19,27 +20,28 @@ function HomePage() {
   };
 
   return (
-    <div className="homepage-img">
-      <div className="container">
-        <div className="homepage-title">
+    <div className='homepage-img'>
+      <div className='container'>
+        <div className='homepage-title'>
           <h1>Cluedo</h1>
           {/* <RxMagnifyingGlass /> */}
         </div>
         <Form onSubmit={handleSub}>
-          <Form.Group className="mb-3" controlId="input">
+          <Form.Group className='mb-3' controlId='input'>
             <Form.Label>What is your name?</Form.Label>
             <Form.Control
               onChange={handleChange}
               value={nickName}
-              type="text"
-              placeholder="Enter nickname..."
+              type='text'
+              placeholder='Enter nickname...'
             />
-            <Form.Text className="text-muted"></Form.Text>
+            <Form.Text className='text-muted'></Form.Text>
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant='primary' type='submit'>
             Solve a murder
           </Button>
         </Form>
+        <SuspectCard />
       </div>
     </div>
   );

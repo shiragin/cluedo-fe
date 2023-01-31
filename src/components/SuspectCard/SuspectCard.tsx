9e7f6b6a271d0910578f21df5a9a11cb6b6c5eb9
odeya2626 <odeyacoh26@gmail.com>
2023-01-31 08:57:44 +0200

    added SuspectCard Component

--- src/components/SuspectCard/SuspectCard.tsx
+++ src/components/SuspectCard/SuspectCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './SuspectCard.css';
 // interface Props {
 //   name: string;
 //   type: string;
@@ -9,15 +9,20 @@ import React from 'react';
 
 // const SuspectCard: React.FC<Props> = ({ name, type, color, image }) => {
   const SuspectCard: React.FC = () => {
+
   const name = 'Yonatan Salmon';
   const type = 'Suspect';
   const img = "https://res.cloudinary.com/dmy0f63gx/image/upload/v1675146408/T041TFU6DE1-U041J6GTJS3-4690e5465a74-512_f1aqiq.jpg"
-
+  const handleClick = () => {
+    console.log('clicked');
+  }
     return (
-      <div className="suspect-card" style={{ backgroundColor: "red" }}>
+      <div className="suspect-card" onClick={handleClick}>
+        <img className="suspect-card-image" src={img} alt={name} />
+        <div className='text-container'>
         <h3 className="suspect-card-name">{name}</h3>
         <p className="suspect-card-type">Type: {type}</p>
-        <img className="suspect-card-image" src={img} alt={name} />
+        </div>
       </div>
     );
   };
